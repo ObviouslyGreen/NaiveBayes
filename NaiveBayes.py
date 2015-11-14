a389e7a2b7c7f04d5cfa07cdd8cf235e493fb764
Michael Chen <myc30894@gmail.com>
2015-11-13 18:17:13 -0600

    Implemented training

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -16,19 +16,34 @@ class NaiveBayes:
         # Model of 10 digits
         # Digits are images with values ' ', '+', or '#'
         self.model = np.zeros((10, SIZE, SIZE, 3))
+        self.num_count = np.zeros(10)
 
-    def train(self):
-        training_data = np.chararray((10, SIZE, SIZE), unicode=True)
+
+    def train(self, k):
         with open(DATA_DIR + '/traininglabels') as f0, open(DATA_DIR + '/trainingimages') as f1:
             for line in f0:
                 curr_num = int(line)
+                self.num_count[curr_num] += 1
                 for row in range(SIZE):
-                    training_data[curr_num][row] = np.array(list(f1.readline().rstrip('\n')))
+                    img_row = (f1.readline().rstrip('\n'))
+                    for col in range(SIZE):
+                        if img_row[col] ==  ' ':
+                            self.model[curr_num][row][col][0] += 1
+                        elif img_row[col] == '+':
+                            self.model[curr_num][row][col][1] += 1
+                        elif img_row[col] == '#':
+                            self.model[curr_num][row][col][2] += 1
 
+        for num in range(10):
+            for row in range(SIZE):
+                for col in range(SIZE):
+                    self.model[num][row][col] += k
+                    self.model[num][row][col] /= (self.num_count[num] + 3 * k)
 
 def main():
     nb = NaiveBayes()
-    nb.train()
+    nb.train(25)
+
 
 
 if __name__ == '__main__':
